686f9638260970f3981584d5a7522d0604779e1a
Isaiah Burkes <idkburkes@gmail.com>
2021-12-22 00:42:58 -0500

    Added required methods to classes

--- game_of_greed/banker.py
+++ game_of_greed/banker.py
@@ -1,4 +1,14 @@
 
 
+
 class Banker:
     pass
+
+    def shelf(self, points):
+        pass
+
+    def bank(self, points):
+        pass
+
+    def clear_shelf(self):
+        pass
--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,3 +1,14 @@
-
+import random
 class GameLogic:
-    pass
+    
+    @staticmethod
+    def roll_dice(num_dice_thrown):
+        dice_list = []
+        for _ in range(num_dice_thrown):
+            dice_list.append(random.randint(1,6))
+        return dice_list
+
+    @staticmethod
+    def calculate_score():
+        pass
+    
